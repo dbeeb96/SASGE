a050fab8937cb433dbd86d04e8f30301583efbb1
dbeeb96 <117094465+dbeeb96@users.noreply.github.com>
2023-01-31 12:46:39 +0000

    tu as oublé le 1

--- apptest/models.py
+++ apptest/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class person(models.model):
     prenom= models.charfield(max_length=30)
-    nom= models.charfield(max_length=30)
+    nom1= models.charfield(max_length=30)
