d986e63f81fe9f8f9f68ac9e4f0bc6aa6ae75efa
Zahra-Barhamienne <123474350+Zahra-Barhamienne@users.noreply.github.com>
2023-01-31 13:02:56 +0000

    moncode

--- apptest/models.py
+++ apptest/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
-class person(models.model):
-    prenom= models.charfield(max_length=30)
-    nom1= models.charfield(max_length=30)
+class person(models.Model):
+    prenom= models.CharField(max_length=30)
+    nom1= models.CharField(max_length=30)
+
+class Reporter(models.Model):
+    full_name= models.CharField(max_length=70)
+
+    def _str_(self):
+        return self.full_name
