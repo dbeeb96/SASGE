1f2e003ef30eb4aa46e480b7e3e80cc3ba69f837
dbeeb96 <dbeeb96@gmail.com>
2023-04-06 13:28:40 +0000

    update

--- comptes/forms.py
+++ comptes/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, PasswordResetForm, Authe
 from django import forms
 from django.forms import EmailField, TextInput, PasswordInput, ImageField
 
-from django.utils.translation import ugettext_lazy as _  # for protected-
+from django.utils.translation import gettext_lazy as _  # for protected-
 
 
 class LoginForm(AuthenticationForm):
@@ -11,59 +11,30 @@ class LoginForm(AuthenticationForm):
 
     AuthenticationForm
 
-
-CHOICES = (
-    ('CEO', 'CEO'),
-    ('Assistance', 'Assistance'),
-    ('DESE', 'DESE'),
-    ('DEF', 'DEF'),
-    ('DIT', 'DIT'),
-    ('SAIN', 'SAIN'),
-    ('DICM', 'DICM' ),
-    ('SLO', 'SLO'),
-    ('DIP', 'DIP'),
-)
-
-
 class SignupForm(UserCreationForm):
     email = forms.EmailField(required=True)
-
-    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Entrer votre nom d'utilisateur :"})
-                               , label='Nom d\'utilisateur')
-
-    prenom = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Entrer votre nom :"})
-                               , label=' Entrer votre prenom')
-
-    nom = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Entrer votre nom :"})
-                               , label=' Entrer votre nom')
-
-
-    email = forms.EmailField(widget=forms.TextInput(attrs={"placeholder": "Enter votre mot de passe : "}))
-
-    password1 = forms.CharField \
-        (widget=forms.TextInput(attrs={"placeholder": "Entrer votre mot de passe", 'type': 'password'}),
-         label=_("Mot de passe"))
-
-    password2 = forms.CharField \
-        (widget=forms.TextInput(attrs={"placeholder": "Confirmer votre mot de passe", 'type': 'password'}),
-         label=_("Confirmer le mot de passe"))
-
-    users = forms.ChoiceField(choices=CHOICES, label='Directions')
-    # La classe Meta
 
     class Meta:
         model = User
         fields = (
 
             'username',
-            'prenom',
-            'nom',
+            'first_name',
+            'last_name',
             'email',
             'password1',
             'password2',
-            'users'
         )
-        # Le help_text va supprimer le text par défaut dans la page d'inscription
-        help_texts = {
-            'username': None,
-        }
+
+    def save(self, commit=True):
+        user = super(SignupForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['prenom']
+        user.last_name = self.cleaned_data['nom']
+        user.email = self.cleaned_data['email']
+        # user.company_name=self.cleaned_data['company_name']
+
+        if commit:
+            user.save()
+
+        return user
+
--- comptes/models.py
+++ comptes/models.py
@@ -1,4 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+
+CHOICES = (
+    ('CEO', 'CEO'),
+    ('Assistance', 'Assistance'),
+    ('DESE', 'DESE'),
+    ('DEF', 'DEF'),
+    ('DIT', 'DIT'),
+    ('SAIN', 'SAIN'),
+    ('DICM', 'DICM'),
+    ('SLO', 'SLO'),
+    ('DIP', 'DIP'),
+)
+
+
+class company(models.Model):  # [Table of companies ]
+    company_name = models.CharField(max_length=100, default='', primary_key=True)
+
+    def __str__(self):
+        return self.company_name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)  # or models.CASCADE
+    Jobtitle = models.CharField(max_length=100, default='', choices=CHOICES)
+    phone = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='image_profile', blank=True)
+    company_name = models.ForeignKey(company, default='', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
